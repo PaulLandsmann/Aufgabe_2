8d9fc8890462a9d04441e294a6cb3859f1087c09
PaulLandsmann <p.landsmann@online-home.de>
2025-11-14 16:51:37 +0100

    P2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,20 @@
+package p2;
+
+public class P2_main {
+	
+	public static void main(String[] args) {
+		
+		int i = 10;
+		boolean b = false;
+		
+		if(i > 10) {
+			b = true;
+		}
+		
+		System.out.println(i);
+		System.out.println(b);
+	
+	
+	}
+
+}
