d8cd945ee34d8893b3fd9cfd9d701d944b92befd
PaulLandsmann <p.landsmann@online-home.de>
2025-11-14 16:57:14 +0100

    P3

---
+++ src/p3/P3_main.java
@@ -0,0 +1,24 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+		int i = 3;
+		int j = 7;
+		int k = 4;
+		
+		
+		if(i < j) {
+			System.out.print("A");
+		}
+		
+		if(i < k) {
+			System.out.print("B");
+		}
+		
+		if(j < k) {
+			System.out.print("C");
+		}
+	}
+
+}
